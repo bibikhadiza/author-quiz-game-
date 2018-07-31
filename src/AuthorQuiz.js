e04c18ec9e2f92710f7693feeedcbeb2eae3e8b0
Khadiza, Bibi <bibikhadiza.wd@gmail.com>
2018-07-31 12:55:38 -0400

    first commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './AuthorQuiz.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="App">
+        AuthorQuiz
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
