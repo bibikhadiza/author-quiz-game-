4b53adfcc7910fa0f065f25afba65e4558a42998
Khadiza, Bibi <bibikhadiza.wd@gmail.com>
2018-07-31 16:30:35 -0400

    Added all component and UI styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg/220px-Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
+        imageSource: 'Goggle Images',
+        books: ['The Adventures of Huckleberry Finn', 
+        "Life of the Mississippi",
+        "Roughing It"
+        ]
+    }, 
+]
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+const state = {
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+}
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 registerServiceWorker();
